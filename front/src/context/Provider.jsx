63ca81158822c87ee2d7e16df46de964ad4dbc0f
Bruno Cabral <bruno.cabral.silva2018@gmail.com>
2022-10-20 23:05:58 -0300

    Criação de estados e exportações do context - básico

--- front/src/context/Provider.jsx
+++ front/src/context/Provider.jsx
@@ -60,8 +60,165 @@ export default function Provider({ children }) {
   const [equipamentos, setEquipamentos] = useState('');
   const [experiencia, setExperiencia] = useState(0);
 
-  const exports = { 
+  const alterInteligencia = (intelig) => setInteligencia(intelig);
+  const alterRaciocinio = (rac) => {setRaciocinio(rac)};
+  const alterPerseveranca = (pers) => setPerseveranca(pers);
+  const alterForca = (forca) => { setForca(forca) };
+  const alterDestreza = (dex) => setDestreza(dex);
+  const alterVigor = (vig) => setVigor(vig);
+  const alterPresenca = (pres) => setPresenca(pres);
+  const alterManipulacao = (man) => setManipulacao(man);
+  const alterAutocontrole = (aut) => setAutocontrole(aut);
 
+  const alterCiencias = (cie) => setCiencias(cie);
+  const alterErudicao = (erud) => setErudicao(erud);
+  const alterInformatica = (inf) => setInformatica(inf);
+  const alterInvestigacao = (inv) => setInvestigacao(inv);
+  const alterMedicina = (med) => setMedicina(med);
+  const alterOcultismo = (ocu) => setOcultismo(ocu);
+  const alterOficios = (of) => setOficios(of);
+  const alterPolitica = (pol) => setPolitica(pol);
+  const alterArmamento = (arm) => setArmamento(arm);
+  const alterArmasDeFogo = (arm) => setArmasDeFogo(arm);
+  const alterBriga = (bri) => setBriga(bri);
+  const alterConducao = (cond) => setConducao(cond);
+  const alterDissimulacao = (diss) => setDissimulacao(diss);
+  const alterEsportes = (esp) => setEsportes(esp);
+  const alterFurto = (fur) => setFurto(fur);
+  const alterSobrevivencia = (sob) => setSobrevivencia(sob);
+  const alterAstucia = (ast) => setAstucia(ast);
+  const alterEmpatia = (emp) => setEmpatia(emp);
+  const alterExpressao = (exp) => setExpressao(exp);
+  const alterIntimidacao = (intim) => setIntimidacao(intim);
+  const alterManha = (man) => setManha(man);
+  const alterPersuacao = (pers) => setPersuacao(pers);
+  const alterSocializacao = (soc) => setSocializacao(soc);
+  const alterTratoCAnimais = (trat) => setTratoCAnimais(trat);
+
+  const alterGloria = (glo) => setGloria(glo);
+  const alterHonra = (hon) => setHonra(hon);
+  const alterPureza = (pur) => setPureza(pur);
+  const alterSabedoria = (sab) => setSabedoria(sab);
+  const alterSagacidade = (sag) => setSagacidade(sag);
+
+  const alterVantagens = (van) => setVantagens(van);
+  const alterVitalidade = (vit) => setForcaDeVitalidade(vit);
+  const alterForcaDeVontade = (fdv) => setForcaDeVontade(fdv);
+  const alterEssencia = (ess) => setEssencia(ess);
+  const alterInstintoPrimitivo = (inst) => setInstintoPrimitivo(inst);
+  const alterHarmonia = (harm) => setHarmonia(harm);
+
+  const alterDesvantagens = (desv) => setDesvantagens(desv);
+  const alterAtaque = (atk) => setAtaque(atk);
+  const alterEquipamentos = (equip) => setEquipamentos(equip);
+  const alterExperiencia = (exp) => setExperiencia(exp);
+
+  const exports = { 
+    inteligencia,
+    raciocinio,
+    perseveranca,
+    forca,
+    destreza,
+    vigor,
+    presenca,
+    manipulacao,
+    autocontrole,  
+    
+    ciencias,
+    erudicao,
+    informatica,
+    investigacao,
+    medicina,
+    ocultismo,
+    oficios,
+    politica,
+    armamento,
+    armasDeFogo,
+    briga,
+    conducao,
+    dissimulacao,
+    esportes,
+    furto,
+    sobrevivencia,
+    astucia,
+    empatia,
+    expressao,
+    intimidacao,
+    manha,
+    persuacao,
+    socializacao,
+    tratoCAnimais,
+   
+    gloria,
+    honra,
+    pureza,
+    sabedoria,
+    sagacidade,
+  
+    vantagens,
+    vitalidade,
+    forcaDeVontade,
+    essencia,
+    instintoPrimitivo,
+    harmonia,
+  
+    desvantagens,
+    ataque,
+    equipamentos,
+    experiencia,
+  
+    alterInteligencia,
+    alterRaciocinio,
+    alterPerseveranca,
+    alterForca,
+    alterDestreza,
+    alterVigor,
+    alterPresenca,
+    alterManipulacao,
+    alterAutocontrole,
+  
+    alterCiencias,
+    alterErudicao,
+    alterInformatica,
+    alterInvestigacao,
+    alterMedicina,
+    alterOcultismo,
+    alterOficios,
+    alterPolitica,
+    alterArmamento,
+    alterArmasDeFogo,
+    alterBriga,
+    alterConducao,
+    alterDissimulacao,
+    alterEsportes,
+    alterFurto,
+    alterSobrevivencia,
+    alterAstucia,
+    alterEmpatia,
+    alterExpressao,
+    alterIntimidacao,
+    alterManha,
+    alterPersuacao,
+    alterSocializacao,
+    alterTratoCAnimais,
+  
+    alterGloria,
+    alterHonra,
+    alterPureza,
+    alterSabedoria,
+    alterSagacidade,
+  
+    alterVantagens,
+    alterVitalidade,
+    alterForcaDeVontade,
+    alterEssencia,
+    alterInstintoPrimitivo,
+    alterHarmonia,
+  
+    alterDesvantagens,
+    alterAtaque,
+    alterEquipamentos,
+    alterExperiencia,
   };
 
   return (
